26679bd86e96e0984049e8c12010327249669dc4
Jon <jonpkehoe@outlook.com>
2023-01-30 12:20:23 +0000

    trinagle rectangle

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,7 +11,8 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-
+		// colorMode(HSB);
+		background(0);
 		
 	}
 
@@ -19,6 +20,16 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-
+		stroke(255);
+		line(10, 10, 100, 100); // x1 y2 y3
+		circle(300, 250, 70);
+		fill(255,0,0);
+		rect(10, 300, 20, 100); // top leftx, 
+		stroke(34);
+		fill(0,255,0);
+		triangle(40, 900, 90, 300, 50, 23); // x1, y1, x2, y2, x3, y3
+		
+		
+		
 	}
 }
