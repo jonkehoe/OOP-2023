26811b4c147356790f41b2a8411dd28f3e98dbae
Jon <jonpkehoe@outlook.com>
2023-02-01 18:21:49 +0000

    mouse usage

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -20,23 +20,25 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		stroke(255);
+		// stroke(255);
 		
 		
-		fill(246,255, 0);
-		circle(250, 250, 420);
+		// fill(246,255, 0);
+		// circle(250, 250, 420);
 		
-		fill(10,250,206);
-		triangle(00, 450, 500, 450, 250, 0);
-		circle(250,250, 100);
-		fill(255,255,255);
-		ellipse(243, 250, 150, 90);
-		fill(0,0,0);
-		circle(245,250, 70);
+		// fill(10,250,206);
+		// triangle(00, 450, 500, 450, 250, 0);
+		// circle(250,250, 100);
+		// fill(255,255,255);
+		// ellipse(243, 250, 150, 90);
+		// fill(0,0,0);
+		// circle(245,250, 70);
 
 
-		background(0);
-		circle(mouseX, mouseY, 10);
+		// background(0);
+		noStroke();
+		fill(255, 0, 0);
+		circle(mouseX, mouseY, 100);
 
 		
 		
